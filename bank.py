d83c527610a9fc6c2a5cd436d1f550bb99b01749
Avinash Kumar <avibca8@gmail.com>
2024-10-14 21:38:22 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,12 +1,15 @@
+#creating a python program to deposit, withdraw and check the balance of a user..
+#defining a class named BankHolder
 class BankHolder:
     def __init__(self,acountholder,balance) :
         self.acountholder = acountholder
-        self.balance = float(balance)
-        
+        self.balance = float(balance) #converting the value to the float
+#creating different functions into the same class
+#three different functions for each purpose
     def withdraw(self,amount):
-        amount= float(amount)
+        amount= float(amount) #converting the value to the float
         if amount>= self.balance:
-            print("besharm jitna chadar hai utna hi per pasaro")
+            print("not enough credit to withdraw")
         elif amount<=0:
             print("invalid amount")
         else:
@@ -16,7 +19,7 @@ class BankHolder:
     def deposit(self,amount):
         amount= float(amount)
         if amount<=0:
-            print("1 rs to deposit kr...")
+            print("cannot deposit less than 1 rs")
         else:
             self.balance += amount
             print(f"successfuly deposited {amount} rs")
@@ -31,16 +34,18 @@ name= input('enter your name : ')
 balance = input('enter the amount you want to deposit first time: ')
 account=BankHolder(name,balance)
 
-while True:
+#creating the options for the user to choose from
+while True: #this loop will run until the user choose to exit
     print("please choose an option to move forward")
     print("1.check balance")
     print("2.deposit")
     print("3.withdraw")
     print("4.exit")
+
+    #taking the choice of the user..
+    user_choice= int(input('enter your choice: '))# in Int format
     
-    user_choice= int(input('enter your choice: '))
-    
-    match user_choice:
+    match user_choice:#using match case function for ease
         
         case 1:
             account.checkbalance()
